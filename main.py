ec7339b4e27ff5665abf4853f60fda4e5931771b
Lev <rogulev0805@gmail.com>
2025-12-12 09:26:15 +0300

    good

--- main.py
+++ main.py
@@ -9,12 +9,12 @@ from src.model_training.details_detection import SegmentationTrainer
 
 def main():
     config = TrainingConfig(
-        model_name=".models/car_damage_segmentation/final",
+        model_name="nvidia/mit-b5",
         data_dir="./data/classification_details",
         output_dir="./models/car_damage_segmentation",
         batch_size=4,
-        num_epochs=30,
-        learning_rate=1e-5,
+        num_epochs=50,
+        learning_rate=1e-5
     )
 
     print("Setting trainer...")
