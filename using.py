268b1c358fbed9b7bc3be1ea80bcb195e609957b
Lev <rogulev0805@gmail.com>
2025-12-16 23:08:45 +0300

    fix: Mask name

--- using.py
+++ using.py
@@ -4,10 +4,10 @@ import matplotlib.pyplot as plt
 
 # Загрузка модели
 config = TrainingConfig()
-detector = CarPartsDetector("./models/car_damage_segmentation/final", config)
+detector = CarPartsDetector("/models/car_damage_segmentation/final_small_dataset", config)
 
 # Предсказание
-mask, image = detector.predict("path")
+mask, image = detector.predict("/home/lev/PycharmProjects/Car_damage_detection_project/data/classification_details/img/IMG_9215.jpg")
 
 # Визуализация
 result = detector.visualize_prediction(image, mask)
