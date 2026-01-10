c8255c3f86f383423be44315a6681f0d48bc3317
Lev <rogulev0805@gmail.com>
2026-01-10 04:15:08 +0300

    add: Интерполяция для формы

--- src/data_preprocessing/augmentations.py
+++ src/data_preprocessing/augmentations.py
@@ -1,4 +1,5 @@
 import albumentations as A
+import cv2
 
 
 def get_color_augmentations():
@@ -20,22 +21,29 @@ def get_color_augmentations():
 
 
 def get_shape_augmentations():
-    """Аугментации для формы изображения"""
-    return A.Compose([
-        A.HorizontalFlip(p=0.5),
-        A.OneOf([
-            A.MotionBlur(p=0.25),
-            A.MedianBlur(blur_limit=3, p=0.25),
-            A.Blur(blur_limit=3, p=0.25),
-        ], p=0.4),
-        A.ShiftScaleRotate(
-            shift_limit=0.1,
-            scale_limit=0.1,
-            rotate_limit=25,
-            p=0.75
-        ),
-        A.OneOf([
-            A.OpticalDistortion(p=0.3),
-            A.GridDistortion(p=0.3),
-        ], p=0.4),
-    ], additional_targets={'image': 'image', 'annotation': 'image'})
+    """Аугментация для формы изображения"""
+    return A.Compose(
+        [
+            A.HorizontalFlip(p=0.5),
+            A.OneOf([
+                A.MotionBlur(p=0.25),
+                A.MedianBlur(blur_limit=3, p=0.25),
+                A.Blur(blur_limit=3, p=0.25),
+            ], p=0.4),
+            A.ShiftScaleRotate(
+                shift_limit=0.1,
+                scale_limit=0.1,
+                rotate_limit=25,
+                p=0.75,
+                interpolation=cv2.INTER_LINEAR,
+                mask_interpolation=cv2.INTER_NEAREST,
+                border_mode=cv2.BORDER_CONSTANT,
+            ),
+            A.OneOf([
+                A.OpticalDistortion(p=0.3),
+                A.GridDistortion(p=0.3),
+            ], p=0.4),
+        ],
+        additional_targets={"annotation": "mask"}
+    )
+
