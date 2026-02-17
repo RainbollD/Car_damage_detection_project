e502612b79c1bc025c40edee5b66fcaae518ae5f
Lev Rogulin <rogulev0805@gmail.com>
2026-02-17 22:22:35 +0300

    reformat

--- using.py
+++ using.py
@@ -2,16 +2,17 @@ import sys
 import os
 
 from src.inference.detect_parts import CarPartsDetector
-from src.config.traning_config import TrainingConfig
+from src.config import TrainingConfig
 import matplotlib.pyplot as plt
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
-path_img = "path_to_image"
+path_img = "dataset/classification_dent/images/Car damages 101.png"
 
 # Загрузка модели
 config = TrainingConfig()
-detector = CarPartsDetector("path_to_model", config)
+detector = CarPartsDetector(
+    "/home/lev/PycharmProjects/Car_damage_detection_project/models/nvidia_mit-b5/only_dent/checkpoint-700", config)
 
 # Предсказание
 mask, image = detector.predict(path_img)
@@ -22,4 +23,4 @@ plt.imshow(result)
 plt.show()
 
 # Сохранение результата визуализации
-plt.imsave(f"path_to_folder_save/{path_img.split("/")[-1]}", result)
+# plt.imsave(f"path_to_folder_save/{path_img.split("/")[-1]}", result)
